049ba721929ef64793a8593f9c1ea059d46c318e
Шепыч <syndicating@yandex.ru>
2023-06-11 01:02:52 +0300

    Main routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('get/file', function () {
+    return 'Получаем файл';
+});
+
+Route::get('upload/file', function () {
+    return 'Заливаем файл';
+});
